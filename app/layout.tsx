71c7f29ae75d17ed55c30de37520c9e3abb45b65
A-Novychenko <A.Novychenko@outlook.com>
2026-06-23 17:01:04 +0300

    add og img

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
+  metadataBase: new URL('https://viknomayster.vercel.app'), 
   title: "Ремонт та встановлення вікон у Києві | ViknoMayster",
   description: "Ремонт та встановлення віконних конструкцій у Києві та передмісті. Безкоштовний виїзд майстра в день звернення. Гарантія на роботи.",
 };
